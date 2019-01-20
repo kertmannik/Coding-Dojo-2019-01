82d25982733115cc971bd6e2c606f1184ea82b2f
kertmannik <kert.mannik@gmail.com>
2019-01-20 07:29:41 +0200

    The TDD string calculator exercise

--- src/main/java/bootstrap/App.java
+++ src/main/java/bootstrap/App.java
@@ -1,8 +1,45 @@
 package bootstrap;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class App {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+    }
+
+    public int add(String numbers) throws Exception {
+        if (numbers.isEmpty()) {
+            return 0;
+        } else {
+            String delimiter = ",";
+            String actualInput = numbers;
+
+            if (numbers.startsWith("//")) {
+                delimiter = numbers.substring(2,3);
+                actualInput = actualInput.substring(3 + delimiter.length());
+            }
+
+             List<String> negativeNumbers = Arrays.stream(actualInput.replace("\n",delimiter)
+                    .split(delimiter))
+                    .filter(number -> number.startsWith("-"))
+                    .collect(Collectors.toList());
+
+            if (0 < negativeNumbers.size()) throw new Exception(String.join(",",negativeNumbers));
+
+            return Arrays.stream(actualInput.replace("\n",delimiter)
+                    .split(delimiter))
+                    .map(Integer::parseInt)
+                    .mapToInt(Integer::intValue)
+                    .filter(number -> isBiggerThan(1000, number))
+                    .sum();
+        }
+    }
+
+    private boolean isBiggerThan(int limit, int number) {
+        return limit > number;
     }
 }
--- src/test/java/bootstrap/AppTest.java
+++ src/test/java/bootstrap/AppTest.java
@@ -1,15 +1,112 @@
 package bootstrap;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class AppTest {
 
     @Test
-    @DisplayName("My first test")
-    void my_first_test() {
-        assertThat("4" + "2").isEqualTo("42");
+    void should_return_zero_when_empty_string() throws Exception {
+        App calculator = new App();
+        String empty_string = "";
+
+        assertThat(calculator.add(empty_string)).isEqualTo(0);
+    }
+
+    @Test
+    void should_return_sum_when_one_number() throws Exception {
+        App calculator = new App();
+        String numbers = "1";
+
+        assertThat(calculator.add(numbers)).isEqualTo(1);
+    }
+
+    @Test
+    void should_return_sum_when_two_numbers() throws Exception {
+        App calculator = new App();
+        String numbers = "1,2";
+
+        assertThat(calculator.add(numbers)).isEqualTo(3);
+    }
+
+
+    @Test
+    void should_return_sum_when_multiple_numbers() throws Exception {
+        App calculator = new App();
+        String numbers = "1,2,88";
+
+        assertThat(calculator.add(numbers)).isEqualTo(91);
+    }
+
+    @Test
+    void should_return_sum_with_new_line_delimiter() throws Exception {
+        App calculator = new App();
+        String numbers = "1\n2,88";
+
+        assertThat(calculator.add(numbers)).isEqualTo(91);
+    }
+
+
+    @Test
+    void should_return_sum_with_configurable_delimiter() throws Exception {
+        App calculator = new App();
+        String numbers = "//;\n1;2";
+
+        assertThat(calculator.add(numbers)).isEqualTo(3);
+    }
+
+    @Test
+    void should_throw_an_error_when_negative_number() {
+        App calculator = new App();
+        String numbers = "//;\n-1;2";
+
+
+        try {
+            calculator.add(numbers);
+            fail("Should throw Exception if negative number");
+        } catch (Exception expected) {
+
+        }
+    }
+
+    @Test
+    void should_throw_an_error_with_message_containing_negative_number() {
+        App calculator = new App();
+        String numbers = "//;\n-1;2";
+
+
+        try {
+            calculator.add(numbers);
+            fail("Should throw Exception if negative number");
+        } catch (Exception expected) {
+            assertThat(expected.getMessage()).isEqualTo("-1");
+        }
+    }
+
+    @Test
+    void should_throw_an_error_when_multiple_negative_numbers() throws Exception {
+        App calculator = new App();
+        String numbers = "//;\n-1;-2";
+
+
+        try {
+            calculator.add(numbers);
+            fail("Should throw Exception if negative number");
+        } catch (Exception expected) {
+            assertThat(expected.getMessage()).isEqualTo("-1,-2");
+        }
+    }
+
+    @Test
+    void should_return_sum_without_large_numbers() throws Exception {
+        App calculator = new App();
+        String numbers = "//;\n1000;2;1000";
+
+        assertThat(calculator.add(numbers)).isEqualTo(2);
     }
 }
+
